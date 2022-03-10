7caab09d8a86eb3af1edbd137ce7d77af074f052
alan <awalan1605@163.com>
2022-03-10 16:28:15 +0800

    add table create sql for eth erc token, address and coin price.

--- createTable/ods_eth_table.sql
+++ createTable/ods_eth_table.sql
@@ -1,7 +1,47 @@
 create external table eth.ods_eth_block (`line` string)
 partitioned by (`dt` string)
-STORED AS
-  INPUTFORMAT 'com.hadoop.mapred.DeprecatedLzoTextInputFormat'
-  OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe' 
+WITH SERDEPROPERTIES ( 
+  'field.delim'='\t', 
+  'line.delim'='\n', 
+  'serialization.format'='\t') 
+STORED AS SEQUENCEFILE
 LOCATION
   'hdfs://hdfscluster/warehouse/eth/ods/ods_eth_block';
+ 
+create external table eth.ods_erc_token (`line` string)
+partitioned by (`dt` string)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe' 
+WITH SERDEPROPERTIES ( 
+  'field.delim'='\t', 
+  'line.delim'='\n', 
+  'serialization.format'='\t') 
+STORED AS SEQUENCEFILE
+LOCATION
+  'hdfs://hdfscluster/warehouse/eth/ods/ods_erc_token';
+ 
+create external table eth.ods_eth_address (`line` string)
+partitioned by (`dt` string)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe' 
+WITH SERDEPROPERTIES ( 
+  'field.delim'='\t', 
+  'line.delim'='\n', 
+  'serialization.format'='\t') 
+STORED AS SEQUENCEFILE
+LOCATION
+  'hdfs://hdfscluster/warehouse/eth/ods/ods_eth_address';
+
+create external table ods_coin_price (`line` string)
+partitioned by (`dt` string)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe' 
+WITH SERDEPROPERTIES ( 
+  'field.delim'='\t', 
+  'line.delim'='\n', 
+  'serialization.format'='\t') 
+STORED AS SEQUENCEFILE
+LOCATION
+  'hdfs://hdfscluster/warehouse/eth/ods/ods_coin_price';
